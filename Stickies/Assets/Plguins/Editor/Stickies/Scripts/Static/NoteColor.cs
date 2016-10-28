f428eeae0fc88d348528774b410ed26ca9054bcf
Taras Leskiv <leskiv.taras@gmail.com>
2016-10-28 21:42:36 +0200

    stuff

---
+++ Stickies/Assets/Plguins/Editor/Stickies/Scripts/Static/NoteColor.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum NoteColor
+{
+    Blue,
+    Green,
+    Pink,
+    Purple,
+    White,
+    Yellow
+}
--- Stickies/Assets/Plguins/Editor/Stickies/Scripts/StickyNoteContent.cs
+++ Stickies/Assets/Plguins/Editor/Stickies/Scripts/StickyNoteContent.cs
@@ -3,14 +3,7 @@ using UnityEditor;
 
 public class StickyNoteContent : PopupWindowContent
 {
-    bool toggle1 = true;
-    bool toggle2 = true;
-    bool toggle3 = true;
-
-    Color m_ColorDark;
-    Color m_ColorGray;
-
-    const float k_Height = 32f;
+    const float ColorPickerHeight = 48f;
 
     string _guid;
 
@@ -21,17 +14,6 @@ public class StickyNoteContent : PopupWindowContent
     public StickyNoteContent(string guid)
     {
         _guid = guid;
-
-        if (EditorGUIUtility.isProSkin)
-        {
-            m_ColorDark = new Color(0.18f, 0.18f, 0.18f);
-            m_ColorGray = new Color(0.43f, 0.43f, 0.43f);
-        }
-        else
-        {
-            m_ColorDark = new Color(0.64f, 0.64f, 0.64f);
-            m_ColorGray = new Color(0.92f, 0.92f, 0.92f);
-        }
 
         m_MiddleCenterStyle = new GUIStyle(EditorStyles.miniLabel) { alignment = TextAnchor.MiddleCenter };
     }
@@ -44,42 +26,21 @@ public class StickyNoteContent : PopupWindowContent
     public override void OnGUI(Rect rect)
     {
         Handles.DrawSolidRectangleWithOutline(rect, Color.yellow, Color.yellow);
-        if (DrawUtils.DrawColorChooser(new Rect(15, 15, 32, 32), Colors.YellowHeader, Colors.YellowBg))
-        {
-            Debug.Log("Color click");
-        }
-        if (DrawUtils.DrawColorChooser(new Rect(15, 15+32, 32, 32), Colors.YellowHeader, Colors.YellowBg))
-        {
-            Debug.Log("Color click");
-        }
-        if (DrawUtils.DrawColorChooser(new Rect(15, 15+32+32, 32, 32), Colors.YellowHeader, Colors.YellowBg))
-        {
-            Debug.Log("Color click");
-        }
+        DrawColorPicker(new Rect(rect.x, rect.y, rect.width, ColorPickerHeight));
 
         editorWindow.Repaint();
     }
 
-//    public void DrawShutterGraph(float angle)
-//    {
-//        var center = GUILayoutUtility.GetRect(128, k_Height).center;
-//
-//        // Parameters used to make transitions smooth.
-//        var zeroWhenOff = Mathf.Min(1f, angle * 0.1f);
-//        var zeroWhenFull = Mathf.Min(1f, (360f - angle) * 0.02f);
-//
-//        // Shutter angle graph
-//        var discCenter = center - new Vector2(k_Height * 2.4f, 0f);
-//        // - exposure duration indicator
-//        DrawDisc(discCenter, k_Height * Mathf.Lerp(0.5f, 0.38f, zeroWhenFull), m_ColorGray);
-//        // - shutter disc
-////        DrawDisc(discCenter, k_Height * 0.16f * zeroWhenFull, m_ColorDark);
-//        // - shutter blade
-////        DrawArc(discCenter, k_Height * 0.5f, 360f - angle, m_ColorDark);
-//        // - shutter axis
-////        DrawDisc(discCenter, zeroWhenOff, m_ColorGray);
-//    }
-
+    void DrawColorPicker(Rect rect)
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            if (DrawUtils.DrawColorChooser(new Rect(15 + i * 32, rect.y, 32, 32), Colors.YellowHeader, Colors.YellowBg))
+            {
+                Debug.Log("Color click");
+            }
+        }
+    }
 
     public override void OnOpen()
     {
