7a0cb4bd6f668d1a9d7adcfbb6a885317a2b1781
Taras Leskiv <leskiv.taras@gmail.com>
2016-10-28 22:17:57 +0200

    colors

--- Stickies/Assets/Plguins/Editor/FileRevealer/README.txt
+++
@@ -1,3 +0,0 @@
-﻿Just import the package into your project and everything will work out of the box. No special setup is required.
-
-Support: leskiv.taras@gmail.com
---
+++ Stickies/Assets/Tester.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tester : MonoBehaviour
+{
+
+    public Color test;
+
+	// Use this for initialization
+	void Start () {
+	    Debug.Log(test);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
