b89ff798faaceaad0fa75eac242f5249eb871623
Taras Leskiv <leskiv.taras@gmail.com>
2016-11-27 20:39:33 +0100

    wip

---
+++ Stickies/Assets/Plugins/Editor/Stickies/Scripts/GUI/Note/NoteHeader.cs
@@ -0,0 +1,13 @@
+﻿#if UNITY_EDITOR
+using System;
+using UnityEngine;
+using UnityEditor;
+
+namespace DeadMosquito.Stickies
+{
+    // TODO Refactor
+    public class NoteHeader
+    {
+    }
+}
+#endif
