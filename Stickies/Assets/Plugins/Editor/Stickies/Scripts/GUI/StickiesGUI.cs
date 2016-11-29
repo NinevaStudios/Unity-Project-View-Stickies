c6c509aee09baacec9d93dcc7042e8466e946957
Taras Leskiv <leskiv.taras@gmail.com>
2016-11-29 21:30:10 +0100

    color chooser

--- Stickies/Assets/Plugins/Editor/Stickies/Scripts/GUI/StickiesGUI.cs
+++ Stickies/Assets/Plugins/Editor/Stickies/Scripts/GUI/StickiesGUI.cs
@@ -28,15 +28,25 @@ namespace DeadMosquito.Stickies
 
         public static NoteColor ColorChooser(Rect rect)
         {
+            const float colorBtnSize = 32f;
+            const float halfBtnSize = colorBtnSize / 2f;
+            const float spacing = 7f;
+
+            int N = Colors.Values.Length;
+            /* width = buttons + spacings + half-button */
+            float colorsRowWidth = N * colorBtnSize + (N - 1) * spacing;
+
+            var x = (rect.width - colorsRowWidth) / 2f + halfBtnSize;
             var colors = Colors.Values;
-            for (int i = 0; i < colors.Length; i++)
+            for (int i = 0; i < colors.Length; i++, x += (colorBtnSize + spacing))
             {
                 var color = colors[i];
                 if (color == NoteColor.None)
                     continue;
 
                 var noteColors = Colors.ColorById(color);
-                if (ColorButton(new Rect(15 + i * 36, rect.y, 32, 32), noteColors.main,
+                const float yOffset = colorBtnSize;
+                if (ColorButton(new Rect(x, rect.y + yOffset, colorBtnSize, colorBtnSize), noteColors.main,
                     noteColors.chooserOutline))
                 {
                     return color;
