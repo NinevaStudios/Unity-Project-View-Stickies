f4a6cb013cf8d0a6b123b5b6abcb0ca2d8818469
Taras Leskiv <leskiv.taras@gmail.com>
2016-12-01 00:08:46 +0100

    Inspector

---
+++ Stickies/Assets/Plugins/Editor/Stickies/Scripts/Storage/Inspector/NoteStorageInspector.cs
@@ -0,0 +1,21 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using System.Linq;
+using UnityEditor;
+
+namespace DeadMosquito.Stickies
+{
+    [CustomEditor(typeof(NoteStorage))]
+    public class NoteStorageInspector : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            EditorGUILayout.HelpBox(
+                "This is file that stores all the stickies data! Do not remove this file or move it around!",
+                MessageType.Warning);
+            GUI.enabled = false;
+            base.OnInspectorGUI();
+        }
+    }
+}
+#endif
